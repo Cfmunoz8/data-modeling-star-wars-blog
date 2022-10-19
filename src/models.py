feb066fa6f60d38641d19613d2c17bdd13333b3a
Cfmunoz8 <cfmunoz8@gmail.com>
2022-10-19 23:04:58 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,40 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User (Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    email = Column(String(250))
+    password = Column(String(10))
+    favorites = relationship("favorites")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Favorites (Base):
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    user_id = Column(Integer, ForeignKey("users.id"))
 
-    def to_dict(self):
-        return {}
+class Character (Base):
+    __tablename__ = "characters"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    homeworld = Column(String(100))
+    vehicle = Column(String(100))
+    planet_id = Column(Integer, ForeignKey("planets.id"))
+    vehicle_id = Column(Integer, ForeignKey("vehicles.id"))
+
+class Planet (Base):
+    __tablename__ = "planets"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    population = Column(Integer)
+
+class Vehicles (Base):
+    __tablename__ = "vehicles"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100))
+    crew = Column(Integer)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
